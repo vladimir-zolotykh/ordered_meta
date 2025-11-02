b8d67aabcd7c31706eb875d7270cf5691b166ef8
Vladimir Zolotykh <vladimir.zolotykh@gmail.com>
2025-11-02 17:54:29 +0200

    Initial import
    Classes Typed (descriptor) , OrderedMeta(type), OrderedStruct, Stock

---
+++ run.py
@@ -0,0 +1,70 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# PYTHON_ARGCOMPLETE_OK
+from collections import OrderedDict
+
+
+class Typed:
+    expected_type = type(None)
+
+    def __init__(self, name):
+        self._name = name
+
+    def __set__(self, instance, value):
+        if not isinstance(value, self.expected_type):
+            raise TypeError("Expected " + self.expected_type)
+        instance.__dict__[self._name] = value
+
+
+class Integer(Typed):
+    expected_type = int
+
+
+class Float(Typed):
+    expected_type = float
+
+
+class String(Typed):
+    expected_type = str
+
+
+class OrderedMeta(type):
+    def __new__(clsname, bases, clsdict):
+        d = dict(clsdict)
+        _order = []
+        for name, value in clsdict.items():
+            if isinstance(value, Typed):
+                value._name = name
+                _order.append[name]
+        d["_order"] = _order
+        return super().__new__(clsname, bases, d)
+
+    @classmethod
+    def __prepare__(cls):
+        return OrderedDict()
+
+
+class OrderedStruct(metaclass=OrderedMeta):
+    def as_csv(self):
+        return ",".join(getattr(self, name) for name in self._order)
+
+
+class Stock(OrderedStruct):
+    name = String()
+    shares = Integer()
+    price = Float()
+
+    def __init__(self, name, shares, price):
+        self.name = name
+        self.shares = shares
+        self.price = price
+
+
+if __name__ == "__main__":
+    stock = Stock("GOOG", 100, 490.1)
+    print(stock.name)
+    print(stock.as_csv())
+    try:
+        stock = Stock("AAPL", "a lot", 610.23)
+    except TypeError as e:
+        print(e)
